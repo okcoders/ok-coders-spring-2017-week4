e91e0ccd94d7537a214edba8c36b8e8134060d34
Bob Rockers <bob@rockerssoft.org>
2017-04-15 12:31:39 -0500

    more cleanup and email routes>

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 var restify = require('restify');
 var server = restify.createServer();
 var port = 8088;
-var fs = require('fs');
+
+var hello = require('./routes/hello.js');
+var emails = require('./routes/emails.js');
+var client = require('./client');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/enron');
 var db = mongoose.connection;
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-	_id: Schema.Types.ObjectId,
-	sender: String,
-	recipients: [],
-	cc: [],
-	text: String,
-	mid: String,
-	fpath: String,
-	bcc: [],
-	to: [],
-	replyto: Schema.Types.Mixed,
-	ctype: String,
-	fname: String,
-	date: Date,
-	subject: String
-});
-var Emails = mongoose.model('emails', schema);
 
 db.on('error', function(msg){
 	console.log('Mongoose connection error %s', msg);
@@ -34,51 +18,15 @@ db.once('open', function(){
 	console.log('Mongoose connection established');
 });
 
-function send(req, res, next){
-	res.send("Testing " + req.params.name);
-	return next();
-}
+// Client
+server.get('/', client.get);
 
-function getEmails(req, res, next){
-	Emails.find().limit(10).exec(function(err,data){
-		if(err){res.send('Error');}
-		else {
-			res.json(data);
-		}
-	});
-	return next();
-}
-
-server.get('/', function(req, res, next){
-	fs.readFile('index.html', function(err,data){
-		if(err){ 
-			console.log("Cannot read file index.html");
-			res.send(404);
-		} else {
-			var body = data.toString();
-			res.writeHead(200, {
-				'Content-Length' : Buffer.byteLength(body),
-				'Content-Type' : 'text/html'
-			});
-			res.write(body);
-			res.end();
-		}
-		return next();
-	});
-});
-
-server.get('/emails', getEmails);
-server.get('/hello/:name', send);
-server.put('/hello/:name', send);
-server.post('/hello/:name', function(req, res, next){
-	res.send(201, req.params.name + "'s random String is: " +
-		Math.random().toString(36).substr(3,8));
-	return next();
-});
-server.del('/hello/:name', function(req, res, next){
-	res.send(req.params.name + " is now gone... deleted");
-	return next();
-});
+// Server Responses
+server.get('/emails', emails.get);
+server.get('/hello/:name', hello.send);
+server.put('/hello/:name', hello.send);
+server.post('/hello/:name', hello.post);
+server.del('/hello/:name', hello.del);
 
 server.listen(port, function(){
 	console.log('%s listening at %s', server.name, port);
