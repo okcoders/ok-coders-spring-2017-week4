0f83ae70f990c5287e493841b2687f143b4d867b
Bob Rockers <bob@rockerssoft.org>
2017-04-15 11:34:22 -0500

    Working client/server setup.

---
+++ README.md
@@ -0,0 +1,15 @@
+// /blogs/posts/comments/
+//
+// CRUD   | mongoDB | REST
+// -------|---------|------
+// create | insert  | POST
+// read   |  find   | GET
+// update | update  | PUT
+// delete | remove  | DELETE
+
+// server.verb('path' function(req, res, next){})
+//
+// -----------Database Setup
+// wget http://jsonstudio.com/wp-content/uploads/2014/02/enron.zip
+// unzip enron.zip
+// mongoimport --db enron --collection emails --drop --file enron.json
--- index.js
+++ index.js
@@ -1,32 +1,82 @@
 var restify = require('restify');
 var server = restify.createServer();
 var port = 8088;
+var fs = require('fs');
 
-// /blogs/posts/comments/
-//
-// CRUD   | mongoDB | REST
-// -------|---------|------
-// create | insert  | POST
-// read   |  find   | GET
-// update | update  | PUT
-// delete | remove  | DELETE
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/enron');
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
 
-// server.verb('path' function(req, res, next){})
+var schema = new Schema({
+	_id: Schema.Types.ObjectId,
+	sender: String,
+	recipients: [],
+	cc: [],
+	text: String,
+	mid: String,
+	fpath: String,
+	bcc: [],
+	to: [],
+	replyto: Schema.Types.Mixed,
+	ctype: String,
+	fname: String,
+	date: Date,
+	subject: String
+});
+var Emails = mongoose.model('emails', schema);
+
+db.on('error', function(msg){
+	console.log('Mongoose connection error %s', msg);
+});
+
+db.once('open', function(){
+	console.log('Mongoose connection established');
+});
+
 function send(req, res, next){
-	res.send("Testing " + req.params.test);
+	res.send("Testing " + req.params.name);
 	return next();
 }
 
-server.get('/', send);
-server.get('/hello/:test', send);
-server.put('/hello/:test', send);
-server.post('/hello/:stuff', function(req, res, next){
-	res.send(201, req.params.stuff + "'s random String is: " +
+function getEmails(req, res, next){
+	Emails.find().limit(10).exec(function(err,data){
+		if(err){res.send('Error');}
+		else {
+			res.json(data);
+		}
+	});
+	return next();
+}
+
+server.get('/', function(req, res, next){
+	fs.readFile('index.html', function(err,data){
+		if(err){ 
+			console.log("Cannot read file index.html");
+			res.send(404);
+		} else {
+			var body = data.toString();
+			res.writeHead(200, {
+				'Content-Length' : Buffer.byteLength(body),
+				'Content-Type' : 'text/html'
+			});
+			res.write(body);
+			res.end();
+		}
+		return next();
+	});
+});
+
+server.get('/emails', getEmails);
+server.get('/hello/:name', send);
+server.put('/hello/:name', send);
+server.post('/hello/:name', function(req, res, next){
+	res.send(201, req.params.name + "'s random String is: " +
 		Math.random().toString(36).substr(3,8));
 	return next();
 });
-server.del('/hello/:test', function(req, res, next){
-	res.send(req.params.test + " is now gone... deleted");
+server.del('/hello/:name', function(req, res, next){
+	res.send(req.params.name + " is now gone... deleted");
 	return next();
 });
 
